e0d9537f5611b282a68766159bee2505391cafd2
Ivan Yovchev <yovchev23@gmail.com>
2020-03-27 11:29:06 +0100

    Added ResNetLayer functionality

--- resnet_layer.py
+++ resnet_layer.py
@@ -6,6 +6,14 @@ from tensorflow.keras.models import Sequential
 
 from layer_wrappers import BatchNorm, Conv
 
+def shortcut_fn(filters, strides, data_format, kernel_size=1):
+    return Conv(
+            filters=filters, 
+            kernel_size=kernel_size, 
+            strides=strides, 
+            data_format=data_format
+        )
+
 class ResNetLayerV2(Layer):
 
     def __init__(
@@ -19,9 +27,8 @@ class ResNetLayerV2(Layer):
             ):
         super(ResNetLayerV2, self).__init__(name=name, **kwargs)
 
-        self.shortcut_fn = shortcut_fn
-        
-        # a single block consists 
+        self.shortcut = None if shortcut_fn is None else shortcut_fn(filters=filters, strides=strides, data_format=data_format)
+         
         self.block = Sequential([
                 BatchNorm(data_format=data_format),
                 ReLU(),
@@ -33,7 +40,7 @@ class ResNetLayerV2(Layer):
 
     def call(self, inputs, training=False):
         output = self.block(inputs, training=training)
-        shortcut = inputs if self.shortcut_fn is None else self.shortcut_fn(inputs, training)
+        shortcut = inputs if self.shortcut is None else self.shortcut(inputs, training=training)
 
         print(output)
         print(shortcut)
@@ -41,6 +48,6 @@ class ResNetLayerV2(Layer):
         return output + shortcut
 
 if __name__ == "__main__":
-    model = ResNetLayerV2(filters=3, strides=1, data_format='channels_last')
+    model = ResNetLayerV2(filters=3, strides=2, data_format='channels_last', shortcut_fn=shortcut_fn)
     test = tf.convert_to_tensor(np.random.normal(size=(3,50,200,3)), dtype=tf.float32)
     print(model.call(test))
